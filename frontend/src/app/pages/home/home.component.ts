51e2ebed9a7c223b9a60622630b237af3ac38db6
Cristian8a <cristian.ochoa@iteso.mx>
2023-12-03 14:53:16 -0600

    publicationscallservice

--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -15,7 +15,7 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    console.log('Successfull access');
   }
 
   routePulication() {
