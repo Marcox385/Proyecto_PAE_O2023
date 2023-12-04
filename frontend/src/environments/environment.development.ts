fcc9464e4f8d873ff18cc5efe5414ffecf124141
Marco Cordero <marcko_385@hotmail.com>
2023-12-04 07:48:25 -0600

    Deleted GPT keys....

--- frontend/src/environments/environment.development.ts
+++ frontend/src/environments/environment.development.ts
@@ -1,5 +1,4 @@
 export const environment = {
     ID_GOOGLE: "522247600194-chak2evtpo2rfa4gkq5enj6c973mmkj0.apps.googleusercontent.com",
-    API_URL: "http://localhost:3200/",
-    GPT_API: "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",
+    API_URL: "http://localhost:3200/"
 }
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     ID_GOOGLE: "522247600194-chak2evtpo2rfa4gkq5enj6c973mmkj0.apps.googleusercontent.com",
-    API_URL: "https://bug-finders-api.onrender.com/",
-    GPT_API: "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",
+    API_URL: "https://bug-finders-api.onrender.com/"
 }
